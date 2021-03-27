f2180256c870a6f7b8f84b0d5986ddc8f733e4f3
BENSTOH <benson.toh@daimler.com>
2021-03-27 18:17:33 +0800

    feat: eslint prettier husky lintstaged setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import './app.css'
 
 function App() {
     return (
         <>
-            <div>Hello World</div>
+            <div>Hello World!!</div>
         </>
     )
 }
